b7277e79e25e55b98ce63715e50ffce13c39c309
Steven Zuluaga <steven000069@hotmail.com>
2022-08-26 22:12:53 -0500

    Validando ingreso de tareas con lodash

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
+import { isEmpty } from "lodash";
 import "./styles/style.css";
 export default function App() {
+  const [task, setTask] = useState("")
+  const addTask = (e) => {
+    e.preventDefault()
+    
+    if (isEmpty(task)) {
+      alert('Task Empty')
+      return
+    }
+
+    console.log(task)
+    setTask("")
+  }
   return (
     <div className="container mt-5">
       <h1 className="text-primary">Tasks</h1>
       <hr />
       <div className="row">
         <div className="col-8">
-          <h4>Lista de tareas</h4>
+          <h4 className="text-center">Lista de tareas</h4>
           <ul className="list-group">
             <li className="list-group-item">
               <span className="lead">Tarea 1</span>
               <button className="btn btn-primary btn-sm float-end ">Eliminar</button>
               <button className="btn btn-danger btn-sm float-end mx-2">Editar</button>
             </li>
-            <li className="list-group-item">
-              <span className="lead">Tarea 1</span>
-              <button className="btn btn-primary btn-sm float-end ">Eliminar</button>
-              <button className="btn btn-danger btn-sm float-end mx-2">Editar</button>
-            </li>
+            
           </ul>
         </div>
         <div className="col-4">
           <h4>Formulario</h4>
-          <form>
+          <form onSubmit={addTask}>
             <input
               type='input'
-              placeholder='Ingrese tarea'
+              placeholder='Ingrese tarea...'
               className='form-control'
+              onChange={(text) => {
+                setTask(text.target.value)
+              }}
+              value={task}
             />
-            <button className='btn btn-dark w-100 mt-2'>Agregar Tarea</button>
+            <button className='btn btn-dark w-100 mt-2'
+              type="submit"
+
+            >Agregar Tarea</button>
           </form>
         </div>
       </div>
