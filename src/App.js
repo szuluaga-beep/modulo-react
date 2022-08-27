da5c68afe88a4320feae5682ca1f840f4e7285bf
Steven Zuluaga <steven000069@hotmail.com>
2022-08-26 22:33:31 -0500

    Agegar tareas y mostrarla en la página

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { useState } from "react";
 
 import { isEmpty } from "lodash";
-import "./styles/style.css";
+import { nanoid } from 'nanoid'
+
 export default function App() {
   const [task, setTask] = useState("")
+  const [tasks, setTasks] = useState([])
+
   const addTask = (e) => {
     e.preventDefault()
-    
+
     if (isEmpty(task)) {
       alert('Task Empty')
       return
     }
 
-    console.log(task)
+    const newTask = {
+      id: nanoid(10),
+      name:task
+    }
+
+    setTasks([...tasks,newTask])
+    console.log(tasks)
     setTask("")
   }
   return (
@@ -23,11 +32,16 @@ export default function App() {
         <div className="col-8">
           <h4 className="text-center">Lista de tareas</h4>
           <ul className="list-group">
-            <li className="list-group-item">
-              <span className="lead">Tarea 1</span>
-              <button className="btn btn-primary btn-sm float-end ">Eliminar</button>
-              <button className="btn btn-danger btn-sm float-end mx-2">Editar</button>
-            </li>
+            {
+              //nos obviamos el return sin las llaves
+              tasks.map(task => (
+                <li className="list-group-item" key={task.id}>
+                  <span className="lead">{task.name}</span>
+                  <button className="btn btn-primary btn-sm float-end ">Eliminar</button>
+                  <button className="btn btn-danger btn-sm float-end mx-2">Editar</button>
+                </li>
+              ))
+              }
             
           </ul>
         </div>
