42b15522aeeba25efd0ccb5fbec7bebdc1a9c9bd
Steven Zuluaga <steven000069@hotmail.com>
2022-09-19 19:21:02 -0500

    Ajustando metodos de firebase

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,19 +1,29 @@
-import { firebaseApp } from "../firebase";
-import firebase from 'firebase'
-import 'firebase/firestore'
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs,addDoc } from "firebase/firestore/lite";
 
 
-const db = firebase.firestore(firebaseApp)
+const firebaseConfig = {
+     apiKey: "AIzaSyDgNCO2wqNrGIaGx3q9o5Glp-wp6cgxBOM",
+     authDomain: "todo-app-b48be.firebaseapp.com",
+     projectId: "todo-app-b48be",
+     storageBucket: "todo-app-b48be.appspot.com",
+     messagingSenderId: "928984842783",
+     appId: "1:928984842783:web:a0b4f0459e853d72d30b2a"
+};
 
-export const getCollection = async (collection) => {
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+
+export const getCollection = async (coll) => {
      const result = {
           statusResponse: false,
           data: null,
           error: null
      }
      try {
-          const data = await db.collection(collection).get()
-
+          const response = collection(db,coll)
+          const data= await getDocs(response)
           const arrayData = data.docs.map(element => ({
                id: element.id, ...element.data()
           }))
@@ -25,14 +35,16 @@ export const getCollection = async (collection) => {
      return result
 }
 
-export const addDocument = async (collection, data) => {
+export const addDocument = async (coll, data) => {
+
      const result = {
           statusResponse: false,
           data: null,
           error: null
      }
      try {
-          const response = await db.collection(collection).add(data)
+          const res = collection(db, coll)
+          const response = await addDoc(res,data)
           result.statusResponse = true
           result.data = { id: response.id }
 
@@ -73,7 +85,7 @@ export const updateDocument = async (collection, id, data) => {
      return result
 }
 
-export const deleteDocument = async(collection,id) => {
+export const deleteDocument = async (collection, id) => {
      const result = {
           statusResponse: false,
           error: null
@@ -81,7 +93,7 @@ export const deleteDocument = async(collection,id) => {
      try {
           await db.collection(collection).doc(id).delete()
           result.statusResponse = true
-          
+
      } catch (error) {
           result.error = error
      }
